2b1478f77d6e20723cf58f89d5e47da923d06a8a
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-09-14 13:53:02 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jordan Talbot";
+    int age = 30;
+    String state = "Colorado";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
