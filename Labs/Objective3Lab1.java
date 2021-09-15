33103ef8746cf1b5bd15b9aa047918ca8e6fbac8
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-09-15 10:10:30 -0600

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 10;
+    num2 = 20;
+    num3 = 30;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+    //This is the same code as objective2lab3?
+  }
+}
