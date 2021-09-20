0d1fbd8141ed295884115cf1f7577e34319c8737
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-09-20 09:46:27 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int n=1; n<=10; n++){
+      System.out.println(n);
+    }
+    for (int m=10; m>=1; m--){
+      System.out.println(m);
+    }
+  }
+}
