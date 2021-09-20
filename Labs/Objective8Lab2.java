cdd37112052039db3a645eed85df963f525e3149
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-09-20 10:23:11 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+    for(int n=1; n<=20; n++){
+      sum = sum + n;      
+    }
+    System.out.println(sum);
+  }
+}
