0456fafc1f2ee774d8ad29fff1ff902e577f504c
Jordan Talbot <jordan.r.talbot@gmail.com>
2021-09-20 11:27:05 -0600

    complete objective9lab3

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true) {
+      printMenu();
+      selection = scanner.nextInt();
+
+      if(selection == 1){
+        System.out.println("Hello Human");
+        System.out.println();
+      }
+      else if(selection == 2){
+        System.out.println("Apples, Bananas, Coconuts");
+        System.out.println();
+      }
+      else{
+        break;
+      }
+
+    }
+    scanner.close();
+  }
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello!");
+    System.out.println("2: List my favorite food");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
